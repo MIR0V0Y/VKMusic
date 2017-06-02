7b65dcdf747f6e024603aa264c84c787d9107a92
Mirovoy <ya.mirovoy@yandex.ru>
2017-06-02 05:47:52 +0300

    v1.0 Refactoring + fix bugs + user friendly

--- Main.java
+++ Main.java
@@ -18,6 +18,7 @@ along with this program.  If not, see http://www.gnu.org/licenses/
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
@@ -30,55 +31,32 @@ import com.mpatric.mp3agic.Mp3File;
 import com.mpatric.mp3agic.UnsupportedTagException;
 
 public class Main {
+	
+	static File savedir;
+	static File cachdir;
 
 	public static void main(String[] args) throws IOException, UnsupportedTagException, InvalidDataException {
 		
-		// Ищем папку с кэшем
-		File cachdir = new File(System.getProperty("user.home"));
-		if (System.getProperty("os.name").indexOf("Windows") != -1){
-			cachdir = new File(System.getProperty("user.home") + "\\AppData\\Local\\Mozilla\\Firefox\\Profiles");
-			cachdir = new File(cachdir.listFiles()[0] + "\\cache2\\entries");
-			if (!cachdir.exists()){
-				System.out.println("Папка с кэшем не найдена. Выход");
-				return;
-			}
-		} else{
-			cachdir = new File(System.getProperty("user.home") + "/.cache/mozilla/firefox");
-			cachdir = new File(cachdir.listFiles()[0] + "/cache2/entries");
-			if (!cachdir.exists()){
-				System.out.println("Папка с кэшем не найдена. Выход");
-				return;
-			}
-		}
-		
-		// Получаем от пользователя папку сохранения
-		File savedir;
-		if (args.length==1){
-			savedir = new File(args[0]);
-			if (!savedir.exists()){
-				System.out.println("Создание новой папки...");
-				if (!savedir.mkdir()){
-					System.out.println("Ошибка создания новой папки, проверьте корректность имени папки");
-					return;
-					}
-			}
-			System.out.println("Папка сохранения : "+savedir.getAbsolutePath());
-		}else{
-			System.out.println("Неправильно указаны параметры работы программы. Выход");
+		cachdir = findCache();
+		if (cachdir == null)
 			return;
-		}
+		
+		savedir = getSave(args);
+		if (savedir == null)
+			return;
 		
 		System.out.println();
 		
-		// Перебираем все файлы, кроме самых коротких
 		File[] files = cachdir.listFiles();
 		int count = 0;
 		for (int i=0; i < files.length; i++){
 			File f = files[i];
-			if (f.length()/(1024*1024)<1)	// Чем меньше размер файла, тем быстрее идет поиск + отсеиваются сторонние звуки (оповещение в VK)
+			/* Чем больше ограничение на размер файла, тем быстрее идет поиск 
+			 * + отсеиваются сторонние звуки (оповещение в VK)
+			 */
+			if (f.length()/(1024*1024)<1)	
 				continue;
 			
-			// Рабочий сет каждого файла
 			byte[] bs = new byte[1000];
 			char[] c = new char[1000];
 			
@@ -86,69 +64,167 @@ public class Main {
 			try(FileInputStream fis = new FileInputStream(f.getPath())){
 				fis.skip(f.length()-1000);
 				fis.read(bs);
-			} catch (IOException ex) {
-				System.out.println(ex.getMessage());
 			}
 			for (int j = 0; j<bs.length; j++)
 				c[j] = (char)bs[j];
-			String head =new String(c);
-			
+			String head = new String(c);
 			
 			if (head.indexOf("audio/") != -1){
 				System.out.println("Найден аудио файл: "+f.getName());
-				count++;
-				// Генерируем имя файла из ID3 тегов
-				String name = new String(f.getName());
-				Mp3File mp3file = new Mp3File(f.getPath());
-				if (mp3file.hasId3v1Tag()) {
-				  ID3v1 id3v1Tag = mp3file.getId3v1Tag();
-				  name = id3v1Tag.getArtist() + " - " + id3v1Tag.getTitle();
-				}
-				if (mp3file.hasId3v2Tag()) {
-					  ID3v2 id3v2Tag = mp3file.getId3v2Tag();
-					  name = id3v2Tag.getArtist()+ " - " +id3v2Tag.getTitle();
-				}
-				if (!mp3file.hasId3v2Tag() && !mp3file.hasId3v1Tag()){
-					System.out.println("Аудиозапись повреждена, не удалось получить название трека");
-					name = "Broken - "+f.getName();
-				}
-				
-				// Сохранение mp3 в папку
-				File newFile = new File(savedir.getPath() +"/"+ name +".mp3");
-				if (newFile.exists()){
-					if (f.length() == newFile.length()){
-						count--;
-						continue; // Файл уже добавлен в коллекцию
-					}else{
-						newFile = new File(savedir.getPath() +"/"+ name +" Copy.mp3"); // в кэше нашлась другая версия этой же мелодии
-						if(newFile.exists()){
-							count--;
-							continue; // Copy-файл уже добавлен в коллекцию
-						}
-					}
-				}
-				FileChannel sourceChannel = new FileInputStream(f.getPath()).getChannel();
-		        try {
-		            FileChannel destChannel = new FileOutputStream(newFile.getPath()).getChannel();
-		            try {
-		                destChannel.transferFrom(sourceChannel, 0, sourceChannel.size());
-		            } finally {
-		                destChannel.close();
-		            }
-		        } finally {
-		            sourceChannel.close();
-		        }
-				
+				// Генерируем имя файла из ID3 тегов и сохраненяем в папку
+				if (saveSong(f, getName(f)))
+					count++;
 			}
-			
 		}
-
 		System.out.println();
 		System.out.println("Сохранено "+count+" новых аудио файлов");
-		
 	}
+	
+	/**
+     * Функция поиска папки с кэшем
+     * Стучиться к стандартному расположению папки
+     * @return папку с кэшем или null если не нашел
+     */
+	static File findCache(){
+		File dir;
+		if (System.getProperty("os.name").indexOf("Windows") != -1){
+			dir = new File(System.getProperty("user.home") + "\\AppData\\Local\\Mozilla\\Firefox\\Profiles");
+			dir = new File(dir.listFiles()[0] + "\\cache2\\entries");
+			if (!dir.exists()){
+				System.out.println("Папка с кэшем не найдена. Выход");
+				return null;
+			}
+		} else{
+			dir = new File(System.getProperty("user.home") + "/.cache/mozilla/firefox");
+			dir = new File(dir.listFiles()[0] + "/cache2/entries");
+			if (!dir.exists()){
+				System.out.println("Папка с кэшем не найдена. Выход");
+				return null;
+			}
+		}
+		return dir;
+	}
+	
+	/**
+     * Функция получения папки сохранения
+     * @param args - агрументы командной строки
+     * Связывается или создаёт указанную папку для сохранения музыки
+     * Если папка не указана - сохраняет в музыкальную папку пользователя
+     * @return папку с кэшем или null
+     */
+	static File getSave(String[] args){
+		File dir;
+		if (args.length==1){
+			dir = new File(args[0]);
+			if (!dir.exists()){
+				System.out.println("Создание новой папки...");
+				if (!dir.mkdir()){
+					System.out.println("Ошибка создания новой папки, проверьте корректность имени папки");
+					return null;
+				}
+			}
+		}else{
+			if (args.length == 0){
+				dir = new File(System.getProperty("user.home")+File.separator+"Music");
+				if (!dir.exists())
+					dir = new File(System.getProperty("user.home")+File.separator+"Music");
+				
+				if (!dir.exists()){
+					System.out.println("Не удалось найти папку для сохранения. Укажите папку явно");
+					return null;
+				}
+			}
+			else{
+				System.out.println("Неправильно указаны параметры работы программы. Выход");
+				return null;
+			}
+		}
+		System.out.println("Папка сохранения : "+dir.getAbsolutePath());
+		return dir;
+	}
+	
+	/**
+     * Функция сохранения файла
+     * @param file - файл предназначеный для сохранения
+     * @param name - имя файла
+     * @return true - файл сохранён, false - файл уже присутствует в коллекции
+	 * @throws IOException 
+     */
+	static Boolean saveSong(File file, String name) throws IOException{
+		File newFile = new File(savedir.getPath() + file.separator + name +".mp3");
+		if (newFile.exists()){
+			if (file.length() == newFile.length())
+				return false;
+			else{
+				newFile = new File(savedir.getPath() + file.separator + name +" Copy.mp3"); // в кэше нашлась другая версия этой же мелодии
+				if(newFile.exists())
+					return false;
+			}
+		}
+		FileChannel sourceChannel = new FileInputStream(file.getPath()).getChannel();
+        try {
+            FileChannel destChannel = new FileOutputStream(newFile.getPath()).getChannel();
+            try {
+                destChannel.transferFrom(sourceChannel, 0, sourceChannel.size());
+            } finally {
+                destChannel.close();
+            }
+        } finally {
+            sourceChannel.close();
+        }
+		return true;
+	}
+	
+	/**
+     * Функция чтения ID3 тэгов
+     * @param file - файл предназначеный для чтения
+     * @return предлагаемое имя файла
+	 * @throws IOException 
+	 * @throws InvalidDataException 
+	 * @throws UnsupportedTagException 
+     */
+	static String getName(File file) throws UnsupportedTagException, InvalidDataException, IOException{
+		String name = new String(file.getName());
+		Mp3File mp3file = new Mp3File(file.getPath());
+		if (mp3file.hasId3v1Tag()) {
+		  ID3v1 id3v1Tag = mp3file.getId3v1Tag();
+		  name = id3v1Tag.getArtist() + " - " + id3v1Tag.getTitle();
+		}
+		if (mp3file.hasId3v2Tag()) {
+			  ID3v2 id3v2Tag = mp3file.getId3v2Tag();
+			  name = id3v2Tag.getArtist()+ " - " +id3v2Tag.getTitle();
+		}
+		name = validName(name);
+		if ((!mp3file.hasId3v2Tag() && !mp3file.hasId3v1Tag()) || name == " - " || name == "null - null"){
+			System.out.println("Аудиозапись повреждена, не удалось получить название трека");
+			name = "Broken - "+file.getName();
+		}
+		return name;
+	}
+	
+	/**
+     * Функция обработки имени файла
+     * Делает имя файла валидным для NTFS под Windows
+     * @param name - предварительное имя файла
+     * @return валидное имя файла
+     */
+	static String validName(String name){
+		name = name.replace('\\', ' ');
+		name = name.replace('/', ' ');
+		name = name.replace('"', ' ');
+		name = name.replace('*', ' ');
+		name = name.replace('?', ' ');
+		name = name.replace('<', ' ');
+		name = name.replace('>', ' ');
+		name = name.replace('|', ' ');
+		name = name.replace(':', ' ');
+		return name;
+	}
+	
 
+	
 }
+
 
 
 
